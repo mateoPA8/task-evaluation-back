3af7a6c3f8ca881b2cc73e9f53d1a1bf1c3cee4f
Your Name <mateo.poquechoque@ucb.edu.bo>
2025-08-22 00:44:30 -0400

    anadiendo el controller en el app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
 import { ControllersModule } from './infrastructure/controllers/controllers.module';
+import { TaskController } from './infrastructure/controllers/tasks/task.controller';
 
 @Module({
   imports: [ControllersModule, UsecasesProxyModule],
-  controllers: [AppController],
+  controllers: [AppController, TaskController],
   providers: [AppService],
 })
 export class AppModule {}
